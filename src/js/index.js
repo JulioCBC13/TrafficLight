c40579d852d030d1a387b0b35c4ba1f7a011d3ea
JulioCBC13 <blandojulio@gmail.com>
2022-12-05 00:56:31 +0000

    lo hice asi por que me parece mas bonito

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
 //import react into the bundle
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Luces from "./component/Luces.js";
+
+
+function Semaforo  ()  {
+    const colors = ["red","yellow","green"];
+    const [ lit, setLit] = useState("red");
+
+    return (
+        <div className="semaforo">
+            <div id="trafficTop"></div>
+            <div id="container">
+            {colors.map((color) => {
+                return <Luces color={color} lit={lit} setLit={setLit}/>
+            })}
+            </div>
+            
+        </div>
+    )
+}
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Semaforo />, document.querySelector("#app"));
